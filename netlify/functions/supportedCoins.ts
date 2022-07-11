a8fb20fa816b1b72b6432efe1ddc2b539cb7a1f8
Aaron Thompson <thompson_ad@outlook.com>
2022-07-11 14:29:10 +0100

    fetch coins on an interval

---
+++ netlify/functions/supportedCoins.ts
@@ -0,0 +1,10 @@
+// UUIDs of supported coins according to coin ranking api
+enum SupportedCoins {
+  USD = "yhjMzLPhuIDl",
+  BTC = "Qwsogvtv82FCd",
+  ETH = "razxDUgYGNAdQ",
+  ADA = "qzawljRxB5bYu",
+}
+
+// e.g. ["yhjMzLPhuIDl", "Qwsogvtv82FCd", "razxDUgYGNAdQ", "qzawljRxB5bYu"]
+export default Object.values(SupportedCoins);
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import useInterval from "./useInterval";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  // Show an error message if it fails to fetch
+  useInterval(
+    () => {
+      fetchSupportedCoins()
+        .then((coins) => {
+          setCoins(coins);
+        })
+        .catch((err) => console.error("ERROR", err));
+    },
+    5000,
+    true
+  );
+
+  const fetchSupportedCoins = () => {
+    return window
+      .fetch("/.netlify/functions/get-coins")
+      .then(async (response) => {
+        const { data: coinsData } = await response.json();
+        if (response.ok) {
+          if (coinsData.length > 0) {
+            return coins;
+          } else {
+            return Promise.reject(new Error(`Coin data not available`));
+          }
+        } else {
+          return Promise.reject("Something went wrong");
+        }
+      });
+  };
+
+  console.log("COINS", coins);
   return (
     <div className="App">
       <header className="App-header">
