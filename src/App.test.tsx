04ab57ae9e6a85232b8d5c15d45af44cbd30ff93
Aaron Thompson <thompson_ad@outlook.com>
2022-07-13 16:51:31 +0100

    chore: export fixtures

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -12,43 +12,7 @@ import ErrorProvider from "./providers/ErrorProvider";
 import userEvent from "@testing-library/user-event";
 import { Asset } from "./core/Asset";
 
-const coinsFixture = {
-  data: [
-    {
-      uuid: "yhjMzLPhuIDl",
-      name: "US Dollar",
-      priceInUsd: "1.00",
-      symbol: "USD",
-      iconUrl: "https://cdn.coinranking.com/kz6a7w6vF/usd.svg",
-    },
-    {
-      uuid: "Qwsogvtv82FCd",
-      name: "Bitcoin",
-      priceInUsd: "19087.76",
-      symbol: "BTC",
-      iconUrl: "https://cdn.coinranking.com/bOabBYkcX/bitcoin_btc.svg",
-    },
-    {
-      uuid: "razxDUgYGNAdQ",
-      name: "Ethereum",
-      priceInUsd: "1022.41",
-      symbol: "ETH",
-      iconUrl: "https://cdn.coinranking.com/rk4RKHOuW/eth.svg",
-    },
-    {
-      uuid: "qzawljRxB5bYu",
-      name: "Cardano",
-      priceInUsd: "0.41",
-      symbol: "ADA",
-      iconUrl: "https://cdn.coinranking.com/ryY28nXhW/ada.svg",
-    },
-  ],
-};
-
-const priceFixture = {
-  status: "success",
-  data: { data: { timestamp: 1657722480, price: "19246.846200288262" } },
-};
+import { coinsFixture, priceFixture } from "./test/fixtures";
 
 // set up server to mock request for get-coins
 const server = setupServer(
---
+++ src/test/fixtures.ts
@@ -0,0 +1,37 @@
+export const coinsFixture = {
+  data: [
+    {
+      uuid: "yhjMzLPhuIDl",
+      name: "US Dollar",
+      priceInUsd: "1.00",
+      symbol: "USD",
+      iconUrl: "https://cdn.coinranking.com/kz6a7w6vF/usd.svg",
+    },
+    {
+      uuid: "Qwsogvtv82FCd",
+      name: "Bitcoin",
+      priceInUsd: "19087.76",
+      symbol: "BTC",
+      iconUrl: "https://cdn.coinranking.com/bOabBYkcX/bitcoin_btc.svg",
+    },
+    {
+      uuid: "razxDUgYGNAdQ",
+      name: "Ethereum",
+      priceInUsd: "1022.41",
+      symbol: "ETH",
+      iconUrl: "https://cdn.coinranking.com/rk4RKHOuW/eth.svg",
+    },
+    {
+      uuid: "qzawljRxB5bYu",
+      name: "Cardano",
+      priceInUsd: "0.41",
+      symbol: "ADA",
+      iconUrl: "https://cdn.coinranking.com/ryY28nXhW/ada.svg",
+    },
+  ],
+};
+
+export const priceFixture = {
+  status: "success",
+  data: { data: { timestamp: 1657722480, price: "19246.846200288262" } },
+};
